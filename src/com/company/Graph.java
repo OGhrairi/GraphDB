ad71ade629693cc2a0dea2dd461363f35c30ffb3
Omar Ghrairi <omarg96@hotmail.co.uk>
2020-11-24 16:43:49 +0000

    first block out of required classes, thoughts included in comments at the top of each

---
+++ src/com/company/Graph.java
@@ -0,0 +1,19 @@
+package com.company;
+/*This class is here to abstract the graph representation. All I/O interactions with graphs should be called through here.
+
+potential methods required:
+-load - constructor that would retrieve graph data from storage
+-create - make a new graph
+-get - could have a bunch of types of this to get all sorts, whole graph, individual edges, individual vertices,
+labels, properties, etc
+-set - as with get, bunch of use cases here, setting edges, setting vertices, setting labels/properties
+-save - store graph in a file
+-traverse - should take an automaton as input, the output could be variable, or set to a certain type
+
+in the case of moving from one graph to another, should this stay instantiated but be cleared? should it be destroyed?
+probably the latter right?
+
+ */
+public class Graph {
+
+}
